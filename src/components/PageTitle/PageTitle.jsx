3c14704488ff37e57d70a0d1b3e0b341610c4498
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-07-18 23:30:17 +0200

    profile

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,7 +1,13 @@
+import { GiSurferVan } from 'react-icons/gi';
 import css from './PageTitle.module.css';
 
 console.log(css);
 
 export const PageTitle = ({ children }) => {
-  return <h1 className={css.title}>{children}</h1>;
+  return (
+    <h1 className={css.title}>
+      <GiSurferVan className={css.icon} />
+      {children}
+    </h1>
+  );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,45 @@
-import clsx from 'clsx';
+import { FaStar } from 'react-icons/fa';
+import { IoLocationOutline } from 'react-icons/io5';
+import { FaHeart } from 'react-icons/fa6';
 import css from './Profile.module.css';
-
-console.log(css);
+import defaultGallery from './default-gallery.png';
 
 export const Profile = ({ adverts: { gallery, name, price, favorite, rating, location } }) => {
-  // const statusClasses = clsx(css.status, {
-  //   [css.isFavorite]: favorite,
-  //   [css.isRetired]: !favorite,
-  // });
+  const profileImage = gallery || defaultGallery;
 
   const statusClass = favorite ? css.isFavorite : css.isRetired;
 
+  const handleClick = () => {
+    console.log(Date.now());
+  };
+
+  const handleEnter = evt => {
+    console.log(evt);
+  };
+
   return (
     <div className={css.container}>
-      <img src={gallery[0]} alt={name} width="310" height="290" />
-      <h2 className={css.title}>{name}</h2>
-      <p>{price}</p>
-      {/* <p className={statusClasses}>{favorite ? 'Favorite' : 'Retired'}</p> */}
-      <p className={statusClass}>{favorite ? 'Favorite' : 'Retired'}</p>
-      <p>{rating}</p>
-      <p>{location}</p>
-      {/* <p>{description}</p> */}
+      <img className={css.card} src={gallery[0]} alt={name} width="310" height="290" />
+      <div>
+        <h2 className={css.title}>{name}</h2>
+        <p className={css.title}>
+          €{price}
+          <span className={statusClass}>
+            <FaHeart className={css.icon} />
+            {favorite ? 'Favorite' : 'Retired'}
+          </span>
+        </p>
+        <p>
+          <FaStar className={css.iconFa} />
+          {rating}
+          <IoLocationOutline className={css.icon} />
+          {location}
+        </p>
+      </div>
+      <p>The pictures shown here are example vehicles of the respective.</p>
+      <button className={css.button} type="button" onClick={handleClick} onMouseEnter={handleEnter}>
+        Show more
+      </button>
     </div>
   );
 };

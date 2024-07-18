59a11684f390acff098ec6fead0304663e2d4db0
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-07-18 17:30:38 +0200

    css.composes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,23 @@
 import clsx from 'clsx';
-import './Profile.css';
+import css from './Profile.module.css';
+
+console.log(css);
 
 export const Profile = ({ adverts: { gallery, name, price, favorite, rating, location } }) => {
-  const statusClasses = clsx('status', {
-    'is-favorite': favorite,
-    'is-retired': !favorite,
-  });
+  // const statusClasses = clsx(css.status, {
+  //   [css.isFavorite]: favorite,
+  //   [css.isRetired]: !favorite,
+  // });
+
+  const statusClass = favorite ? css.isFavorite : css.isRetired;
+
   return (
-    <div className="profile-container">
+    <div className={css.container}>
       <img src={gallery[0]} alt={name} width="310" height="290" />
-      <h2>{name}</h2>
+      <h2 className={css.title}>{name}</h2>
       <p>{price}</p>
-      <p className={statusClasses}>{favorite ? 'Favorite' : 'Retired'}</p>
+      {/* <p className={statusClasses}>{favorite ? 'Favorite' : 'Retired'}</p> */}
+      <p className={statusClass}>{favorite ? 'Favorite' : 'Retired'}</p>
       <p>{rating}</p>
       <p>{location}</p>
       {/* <p>{description}</p> */}
